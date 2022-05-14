47df75179b41be86b273682e58ba5e8652420321
SudharsanSandyy007 <sudharsandevakumar@gmail.com>
2022-05-14 18:50:13 +0530

    Updates

---
+++ precache-manifest.e6b1b0b15897fb66af765a7d437f6d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f82b522568a7ba2e20d252caeac4da5",
+    "url": "/t/index.html"
+  },
+  {
+    "revision": "a1bc24edd983ad2e16d7",
+    "url": "/t/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "7087592dbcec928c754b",
+    "url": "/t/static/js/2.984d834c.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/t/static/js/2.984d834c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bc24edd983ad2e16d7",
+    "url": "/t/static/js/main.b04369f3.chunk.js"
+  },
+  {
+    "revision": "e7bdf5c47e80bbd6f2ee",
+    "url": "/t/static/js/runtime-main.fd642cb6.js"
+  }
+]);
